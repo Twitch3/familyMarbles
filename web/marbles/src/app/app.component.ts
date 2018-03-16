00f317b734b77a32f2cae1884adea9df34571820
Twitch3 <unconfigured@null.spigotmc.org>
2018-03-16 17:19:57 -0500

    Update to move more towards a Class based structure - Removed angular components in favor of traditional classes to house   game-based information and status. - Added classes in place of removed angular components. - Small tweaks to game menu.

--- web/marbles/src/app/app.component.ts
+++ web/marbles/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  public settingsReady: boolean = true;
+  title = 'app'; // TODO - update unit tests when more functionality exist
 }
--- web/marbles/src/app/app.module.ts
+++ web/marbles/src/app/app.module.ts
@@ -4,11 +4,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component';
-import { BoardSectionComponent } from './components/board-section/board-section.component';
-import { SpaceComponent } from './components/space/space.component';
-import { PlayerComponent } from './components/player/player.component';
-import { TeamComponent } from './components/team/team.component';
-import { MarbleComponent } from './components/marble/marble.component';
 import { GameMenuComponent } from './components/game-menu/game-menu.component';
 import { JoinGameComponent } from './components/game-menu/components/join-game/join-game.component';
 import { CreateGameComponent } from './components/game-menu/components/create-game/create-game.component';
@@ -18,11 +13,6 @@ import { CreateGameComponent } from './components/game-menu/components/create-ga
   declarations: [
     AppComponent,
     BoardComponent,
-    BoardSectionComponent,
-    SpaceComponent,
-    PlayerComponent,
-    TeamComponent,
-    MarbleComponent,
     GameMenuComponent,
     JoinGameComponent,
     CreateGameComponent
